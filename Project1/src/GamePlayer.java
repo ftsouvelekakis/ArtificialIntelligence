e9bc85690163d404410d6140144f220740c292ce
ftsouvelekakis <fotistsouv95@gmail.com>
2020-11-28 20:48:46 +0200

    Implement GamePlayer constructors

--- Project1/src/GamePlayer.java
+++ Project1/src/GamePlayer.java
@@ -1,7 +1,13 @@
 public class GamePlayer{
 
+    private int playerLetter;
+    
     public GamePlayer(){
-
+        
+    }
+    
+    public GamePlayer(int playerLetter){
+        this.playerLetter = playerLetter;
     }
 
 }

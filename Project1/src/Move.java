42243d8920531648d718f0507e4ac7787020d7eb
ftsouvelekakis <fotistsouv95@gmail.com>
2020-11-28 21:17:16 +0200

    Add Move get methods

--- Project1/src/Move.java
+++ Project1/src/Move.java
@@ -31,5 +31,21 @@ public class Move
 		this.col = col;
 		this.value = value;
 	}
+    
+    public int getRow()
+	{
+		return row;
+	}
+	
+	public int getCol()
+	{
+		return col;
+	}
+	
+	public int getValue()
+	{
+		return value;
+	}
+	
 	
 }

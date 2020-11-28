48c645ffff36f89490cf633b39452c5ad9e9c513
ftsouvelekakis <fotistsouv95@gmail.com>
2020-11-28 21:09:12 +0200

    Implement Board constructors

--- Project1/src/Board.java
+++ Project1/src/Board.java
@@ -4,9 +4,36 @@ public class Board {
     public static int O = -1;
     public static int EMPTY = 0;
 
+    private Move lastMove;
+	private int lastLetterPlayed;
+    private int [][] gameBoard;
+
     public Board(){
-        
+        lastMove = new Move();
+		lastLetterPlayed = O;
+		gameBoard = new int[8][8];
+		for(int i=0; i<8; i++)
+		{
+			for(int j=0; j<8; j++)
+			{
+				gameBoard[i][j] = EMPTY;
+			}
+		}
     }
+
+    public Board(Board board)
+	{
+		lastMove = board.lastMove;
+		lastLetterPlayed = board.lastLetterPlayed;
+		gameBoard = new int[8][8];
+		for(int i=0; i<8; i++)
+		{
+			for(int j=0; j<8; j++)
+			{
+				gameBoard[i][j] = board.gameBoard[i][j];
+			}
+		}
+	}
 
 
 }

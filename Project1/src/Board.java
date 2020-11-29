97482991ac4df9543a75a3b86f9b469caca807df
ftsouvelekakis <fotistsouv95@gmail.com>
2020-11-30 00:18:09 +0200

    Add isValidMove() method (not finished) && print valid moves

--- Project1/src/Board.java
+++ Project1/src/Board.java
@@ -92,6 +92,82 @@ public class Board {
 		lastLetterPlayed = moveLetter;
 	}
 
+    public boolean isValidMove(int row, int col)
+	{
+        if(row >= 0 && row < 8 && col >= 0 && col < 8 ){
+            if(gameBoard[row][col] == EMPTY){
+                if(lastLetterPlayed == X){
+                    return false;
+                }
+                else if(lastLetterPlayed == O){
+					if(col == 0 && row == 0){
+						return scanRowRight(row,col) || scanColDown(row, col);
+					}
+					else if(col>0 && col<7 && row>0 && row<7){
+						return (scanRowRight(row,col) || scanRowLeft(row,col) || scanColDown(row, col) || scanColUp(row, col));
+					}
+					else if(col == 7 && row == 7){
+						return scanRowLeft(row,col) || scanColUp(row, col);
+					}
+                }
+            }
+        }
+        return false;
+	}
+
+	public boolean scanRowRight(int row,int col){
+		if(gameBoard[row][col]==EMPTY && gameBoard[row][col+1]==O){	
+			while((col+2)<=7){
+				if(gameBoard[row][col]==X){
+					return true;
+				}
+				col=col+1;
+			}
+		}
+		return false;
+	}
+
+	public boolean scanRowLeft(int row,int col){
+		if(gameBoard[row][col]==EMPTY && gameBoard[row][col-1]==O){	
+			while((col-2)>=0){
+				if(gameBoard[row][col]==X){
+					return true;
+				}
+				col=col-1;
+			}
+		}
+		return false;
+	}
+
+	
+	public boolean scanColDown(int row,int col){
+		if(gameBoard[row][col]==EMPTY && gameBoard[row+1][col]==O){	
+			while((row+2)<=7){
+				if(gameBoard[row][col]==X){
+					return true;
+				}
+				row=row+1;
+			}
+		}
+		return false;
+	}
+
+	public boolean scanColUp(int row,int col){
+		if(gameBoard[row][col]==EMPTY && gameBoard[row-1][col]==O){	
+			while((row-2)>=0){
+				if(gameBoard[row][col]==X){
+					return true;
+				}
+				row=row-1;
+			}
+		}
+		return false;
+	}
+
+
+
+
+
     public void print()
 	{
 		System.out.println("  A B C D E F G H  ");
@@ -108,12 +184,18 @@ public class Board {
 					case O:
 						System.out.print("O ");
 						break;
-					case EMPTY:
-						System.out.print("- ");
+                    case EMPTY:
+                        if(isValidMove(row, col))
+                        {
+                            System.out.print("_ ");
+                        }else
+                        {
+                            System.out.print("- ");
+                        }
 						break;
 					default:
 						break;
-				}
+                }
 			}
 			System.out.println(row);
 		}

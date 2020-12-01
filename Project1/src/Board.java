15b738ce434e3ed158a7816e399c747f3f95c526
ftsouvelekakis <fotistsouv95@gmail.com>
2020-12-01 14:31:44 +0200

    Add flip() methods and some minor changes at scan() methods

--- Project1/src/Board.java
+++ Project1/src/Board.java
@@ -1,10 +1,12 @@
+
+
 public class Board {
     
     public static final int X = 1;
     public static final int O = -1;
-    public static final int EMPTY = 0;
+	public static final int EMPTY = 0;
 
-    private Move lastMove;
+	private Move lastMove;
 	private int lastLetterPlayed;
     private int [][] gameBoard;
 
@@ -90,6 +92,132 @@ public class Board {
 		gameBoard[row][col] = moveLetter;
 		lastMove = new Move(row, col);
 		lastLetterPlayed = moveLetter;
+		flipRowRight(row,col,moveLetter);
+		flipRowLeft(row, col, moveLetter);
+		flipColDown(row, col, moveLetter);
+		flipColUp(row, col, moveLetter);
+		flipDiag0Up(row, col, moveLetter);
+		flipDiag0Down(row, col, moveLetter);
+		flipDiag1Down(row, col, moveLetter);
+		flipDiag1Up(row, col, moveLetter);
+
+	}
+
+	public void flipRowRight(int row,int col,int moveLetter){
+		if(col<=5){
+			if(gameBoard[row][col+1]!=EMPTY){
+				if(scanRow(row, col, moveLetter)){
+					while(gameBoard[row][col+1]!=moveLetter){
+						gameBoard[row][col+1]=moveLetter;
+						col++;
+					}
+				}
+			}
+		}
+	}
+
+	public void flipRowLeft(int row,int col,int moveLetter){
+		if(col>=2){
+			if(gameBoard[row][col-1]!=EMPTY){
+				if(scanRow(row, col, moveLetter)){
+					while(gameBoard[row][col-1]!=moveLetter){
+						gameBoard[row][col-1]=moveLetter;
+						col--;
+					}
+				}
+			}
+		}
+	}
+
+	public void flipColDown(int row,int col, int moveLetter){
+		if(row<=5){	
+			if(gameBoard[row+1][col]!=EMPTY){	
+				if(scanCol(row, col, moveLetter)){
+					while(gameBoard[row+1][col]!=moveLetter){
+						gameBoard[row+1][col]=moveLetter;
+						row++;
+					}
+				}
+			}
+		}
+	}
+
+	public void flipColUp(int row,int col, int moveLetter){	
+		if(row>=2){	
+			if(gameBoard[row-1][col]!=EMPTY){	
+				if(scanCol(row, col, moveLetter)){
+					while(gameBoard[row-1][col]!=moveLetter){
+						gameBoard[row-1][col]=moveLetter;
+						row--;
+					}
+				}
+			}
+		}
+	}
+		
+	public void flipDiag0Down(int row,int col,int moveLetter){
+		if(row<=5 && col<=5){
+			if(gameBoard[row+1][col+1]!=EMPTY){
+				if(scanDiag0(row, col, moveLetter)){
+					while(gameBoard[row+1][col+1]!=moveLetter){	
+						gameBoard[row+1][col+1]=moveLetter;
+						col++;
+						row++;
+					}
+				}
+			}
+		}
+	}
+
+	public void flipDiag0Up(int row,int col,int moveLetter){
+		if(row>=2&&col>=2){	
+			if(gameBoard[row-1][col-1]!=EMPTY){	
+				if(scanDiag0(row, col, moveLetter)){
+					while(gameBoard[row-1][col-1]!=moveLetter){	
+						gameBoard[row-1][col-1]=moveLetter;
+						col--;
+						row--;
+					}
+				}
+			}
+		}
+	}
+
+	public void flipDiag1Down(int row,int col,int moveLetter){
+		if(row<=5&&col>=2){
+			if(gameBoard[row+1][col-1]!=EMPTY){
+				if(scanDiag1(row, col, moveLetter)){
+					while(gameBoard[row+1][col-1]!=moveLetter){	
+						gameBoard[row+1][col-1]=moveLetter;
+						col--;
+						row++;
+					}
+				}
+			}
+		}
+	}
+
+	public void flipDiag1Up(int row,int col,int moveLetter){
+		if(row>=2&&col<=5){
+			if(gameBoard[row-1][col+1]!=EMPTY){
+				if(scanDiag1(row, col, moveLetter)){
+					while(gameBoard[row-1][col+1]!=moveLetter){	
+						gameBoard[row-1][col+1]=moveLetter;
+						col++;
+						row--;
+					}
+				}
+			}
+		}
+	}
+
+	public int opponent(int player){
+		if (player == 1){
+			return -1;
+		}
+		else{
+			return 1;
+		}
 	}
 
     public boolean isValidMove(int row, int col)
@@ -97,32 +225,51 @@ public class Board {
         if(row >= 0 && row < 8 && col >= 0 && col < 8 ){
             if(gameBoard[row][col] == EMPTY){
                 if(lastLetterPlayed == X){
-                    return (scanRow(row, col,O,X)||scanCol(row, col,O,X)||scanDiag0(row,col,O,X)||scanDiag1(row, col,O,X));
+                    return (scanRow(row, col,O)||scanCol(row, col,O)||scanDiag0(row,col,O)||scanDiag1(row, col,O));
                 }
                 else if(lastLetterPlayed == O){
-					return (scanRow(row, col,X,O)||scanCol(row, col,X,O)||scanDiag0(row,col,X,O)||scanDiag1(row, col,X,O));
+					return (scanRow(row, col,X)||scanCol(row, col,X)||scanDiag0(row,col,X)||scanDiag1(row, col,X));
                 }
             }
         }
         return false;
 	}
+	public boolean scanRow(int row,int col,int player){
+		return (scanRowRight(row, col, player)||scanRowLeft(row, col, player));
+	}
+	public boolean scanCol(int row,int col,int player){
+		return (scanColDown(row, col, player)||scanColUp(row, col, player));
+	}
+	public boolean scanDiag0(int row,int col,int player){
+		return (scanDiag0Down(row, col, player)||scanDiag0Up(row, col, player));
+	}
+	public boolean scanDiag1(int row,int col,int player){
+		return (scanDiag1Down(row, col, player)||scanDiag1Up(row, col, player));
+	}
 
-	public boolean scanRow(int row,int col,int player,int opponent){
+	public boolean scanRowRight(int row,int col,int player){
 		if(col<7){
-			if(gameBoard[row][col+1]==opponent){	
+			if(gameBoard[row][col+1]==opponent(player)){	
 				while((col+2)<=7){
-					if(gameBoard[row][col]==player){
+					if(gameBoard[row][col+2]==player){
 						return true;
+					}else if(gameBoard[row][col+2]==EMPTY){
+						break;
 					}
 					col=col+1;
 				}
 			}
 		}
+		return false;
+	}
+	public boolean scanRowLeft(int row,int col,int player){
 		if(col>0){
-			if(gameBoard[row][col-1]==opponent){	
+			if(gameBoard[row][col-1]==opponent(player)){	
 				while((col-2)>=0){
-					if(gameBoard[row][col]==player){
+					if(gameBoard[row][col-2]==player){
 						return true;
+					}else if(gameBoard[row][col-2]==EMPTY){
+						break;
 					}
 					col=col-1;
 				}
@@ -131,22 +278,29 @@ public class Board {
 		return false;
 	}
 
-	public boolean scanCol(int row,int col,int player,int opponent){
+	public boolean scanColDown(int row,int col,int player){
 		if(row<7){
-			if(gameBoard[row+1][col]==opponent){	
+			if(gameBoard[row+1][col]==opponent(player)){	
 				while((row+2)<=7){
-					if(gameBoard[row][col]==player){
+					if(gameBoard[row+2][col]==player){
 						return true;
+					}else if(gameBoard[row+2][col]==EMPTY){
+						break;
 					}
 					row=row+1;
 				}
 			}
 		}
+		return false;
+	}
+	public boolean scanColUp(int row,int col,int player){
 		if(row>0){
-			if(gameBoard[row-1][col]==opponent){	
+			if(gameBoard[row-1][col]==opponent(player)){	
 				while((row-2)>=0){
-					if(gameBoard[row][col]==player){
+					if(gameBoard[row-2][col]==player){
 						return true;
+					}else if(gameBoard[row-2][col]==EMPTY){
+						break;
 					}
 					row=row-1;
 				}
@@ -155,23 +309,30 @@ public class Board {
 		return false;
 	}
 
-	public boolean scanDiag0(int row,int col,int player,int opponent){
+	public boolean scanDiag0Down(int row,int col,int player){
 		if(row<7 && col<7){
-			if(gameBoard[row+1][col+1]==opponent){	
+			if(gameBoard[row+1][col+1]==opponent(player)){	
 				while((row+2)<=7 && (col+2)<=7){
-					if(gameBoard[row][col]==player){
+					if(gameBoard[row+2][col+2]==player){
 						return true;
+					}else if(gameBoard[row+2][col+2]==EMPTY){
+						break;
 					}
 					row=row+1;
 					col=col+1;
 				}
 			}
 		}
+		return false;
+	}
+	public boolean scanDiag0Up(int row,int col,int player){
 		if(row>0 && col>0){
-			if(gameBoard[row-1][col-1]==opponent){	
+			if(gameBoard[row-1][col-1]==opponent(player)){	
 				while((row-2)>=0 && (col-2)>=0){
-					if(gameBoard[row][col]==player){
+					if(gameBoard[row-2][col-2]==player){
 						return true;
+					}else if(gameBoard[row-2][col-2]==EMPTY){
+						break;
 					}
 					row=row-1;
 					col=col-1;
@@ -181,23 +342,30 @@ public class Board {
 		return false;
 	}
 
-	public boolean scanDiag1(int row,int col,int player,int opponent){
+	public boolean scanDiag1Down(int row,int col,int player){
 		if(row<7 && col>0){
-			if(gameBoard[row+1][col-1]==opponent){	
+			if(gameBoard[row+1][col-1]==opponent(player)){	
 				while((row+2)<=7 && (col-2)>=0){
-					if(gameBoard[row][col]==player){
+					if(gameBoard[row+2][col-2]==player){
 						return true;
+					}else if(gameBoard[row+2][col-2]==EMPTY){
+						break;
 					}
 					row=row+1;
 					col=col-1;
 				}
 			}
 		}
+		return false;
+	}
+	public boolean scanDiag1Up(int row,int col,int player){
 		if(row>0 && col<7){
-			if(gameBoard[row-1][col+1]==opponent){	
+			if(gameBoard[row-1][col+1]==opponent(player)){	
 				while((row-2)>=0 && (col+2)<=7){
-					if(gameBoard[row][col]==player){
+					if(gameBoard[row-2][col+2]==player){
 						return true;
+					}else if(gameBoard[row-2][col+2]==EMPTY){
+						break;
 					}
 					row=row-1;
 					col=col+1;
@@ -209,7 +377,7 @@ public class Board {
 
     public void print()
 	{
-		System.out.println("  A B C D E F G H  ");
+		System.out.println("  0 1 2 3 4 5 6 7  ");
 		for(int row=0; row<8; row++)
 		{
 			System.out.print(row + " ");
@@ -226,7 +394,7 @@ public class Board {
                     case EMPTY:
                         if(isValidMove(row, col))
                         {
-                            System.out.print("_ ");
+                            System.out.print("+ ");
                         }else
                         {
                             System.out.print("- ");
@@ -238,6 +406,21 @@ public class Board {
 			}
 			System.out.println(row);
 		}
-		System.out.println("  A B C D E F G H  ");
+		System.out.println("  0 1 2 3 4 5 6 7  ");
 	}
+
+	public boolean isTerminal()
+    {
+        for(int row=0; row<8; row++)
+		{
+			for(int col=0; col<8; col++)
+			{
+				if(gameBoard[row][col] == EMPTY)
+				{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
